9c635e27876760ad029328e39cffda1c230a6eda
Kundan.Kumar <Kundan.Kumar@iinterchange.com>
2025-11-23 19:54:08 +0530

    CURD Op on Employee

---
+++ All_Other_repo/EmployeeOP/src/main/java/com/testtest/op/employeeop/EmployeeOpApplication.java
@@ -0,0 +1,13 @@
+package com.testtest.op.employeeop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeOpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeOpApplication.class, args);
+	}
+
+}
---
+++ All_Other_repo/EmployeeOP/src/test/java/com/testtest/op/employeeop/EmployeeOpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testtest.op.employeeop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeOpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
