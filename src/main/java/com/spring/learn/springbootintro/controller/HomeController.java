999088152a5572895840c088b0290aebbaf3b9b8
Kundan.Kumar <Kundan.Kumar@iinterchange.com>
2025-11-23 14:26:08 +0530

    Params and pathvariables

--- src/main/java/com/spring/learn/springbootintro/controller/HomeController.java
+++ src/main/java/com/spring/learn/springbootintro/controller/HomeController.java
@@ -1,15 +1,35 @@
 package com.spring.learn.springbootintro.controller;
 
+import com.spring.learn.springbootintro.model.User;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
-@ResponseBody
+@RestController
 public class HomeController {
 
     @RequestMapping("/")
     public String home(){
         return "Hello world";
     }
+
+//    @RequestMapping(value = "/user", method = RequestMethod.POST)
+    @GetMapping("/user")
+    public User user(){
+        User user = new User();
+        user.setId("1");
+        user.setName("Kundan");
+        user.setEmailId("kundankumar@gmail.com");
+        return user;
+    }
+
+    @GetMapping("/{id}")
+    public String pathVariable( @PathVariable String id){
+        return "The path variable is : " + id;
+    }
+
+    @GetMapping("/requestParam")
+    public String requestParams(@RequestParam String name, @RequestParam(required = false) String email){
+        return "The request params is : " + name + "email is : " + email;
+    }
+
 }
